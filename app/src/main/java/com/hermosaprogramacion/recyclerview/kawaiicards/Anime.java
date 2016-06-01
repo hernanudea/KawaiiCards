bc4f2526cb715fbeda79b7344d87106b8f75b40e
Hernan Velasquez <hernanudea@gmail.com>
2016-06-01 14:46:38 -0500

    Initial commit

---
+++ app/src/main/java/com/hermosaprogramacion/recyclerview/kawaiicards/Anime.java
@@ -0,0 +1,28 @@
+package com.hermosaprogramacion.recyclerview.kawaiicards;
+
+/**
+ * Created by hernan on 01/06/2016.
+ */
+public class Anime {
+    private int imagen;
+    private String nombre;
+    private int visitas;
+
+    public Anime(int imagen, String nombre, int visitas) {
+        this.imagen = imagen;
+        this.nombre = nombre;
+        this.visitas = visitas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getVisitas() {
+        return visitas;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+}
